5cce2418e021090f60d8ad460e264e516a623c76
jabhax <justiniabarros@gmail.com>
2020-07-31 02:33:58 -0700

    changed db path

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -78,7 +78,8 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': replace(os.path.abspath(os.path.join(BASE_DIR, 'db.sqlite3'))),
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': 'jab',
         'PASSWORD': 'jabbers123',
     }
