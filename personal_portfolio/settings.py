783acda911bd1d2a72f7f1ccacf712d9b4b79414
jabhax <justiniabarros@gmail.com>
2020-07-31 03:40:43 -0700

    reverted db name

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -82,9 +82,7 @@ def replace(path):
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': replace(os.path.abspath(os.path.join(BASE_DIR, 'db.sqlite3'))),
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'jab$db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'USER': 'jab',
         'PASSWORD': 'jabbers123',
     }
